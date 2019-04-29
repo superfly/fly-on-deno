b06b5f9a38fcd2d9529e42e69a72b56634d66585
Kurt <mrkurt@gmail.com>
2019-04-29 17:05:07 -0500

    Proxy kinda works with local Request/Response objects as globals

--- aws.ts
+++ aws.ts
@@ -1,3 +1,4 @@
-import { awsS3 } from "https://unpkg.com/@fly/edge@0.8.0/lib/backends/aws_s3.js";
+import { AwsV4Signer } from "https://unpkg.com/aws4fetch@1.0.0/dist/aws4fetch.esm.js";
+//import { awsS3 } from "https://unpkg.com/@fly/edge@0.8.0/lib/backends/aws_s3.js";
 
-console.log(awsS3);
+console.log(AwsV4Signer);
